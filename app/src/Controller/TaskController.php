0a02fc980e657d7c0f3512717b664b207109d07c
martynas <martynas@harba.co>
2021-02-26 15:10:56 +0200

    task entity and controller

---
+++ app/src/Controller/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/task", name="task", methods={"GET"})
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+}
