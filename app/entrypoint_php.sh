6133d416c0e87e312f711e7f6134ab6c41e4e068
martynas <martynas@harba.co>
2021-04-21 14:50:44 +0300

    added node entrypoint script

--- app/entrypoint_php.sh
+++ app/entrypoint_php.sh
@@ -2,6 +2,8 @@
 
 php /usr/bin/composer install
 
+php bin/console --no-interaction doctrine:migrations:migrate
+
 # print env variables for cron
 printenv | grep -v "no_proxy" >> /etc/environment
 
